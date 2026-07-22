87234a4fa35c9468631597f8e34fa17ac41da655
SandaruwanWeerawardhana <weerawardana@gmail.com>
2026-07-22 12:38:33 +0530

    feat: Initialize BeyosclothingApplication with Spring Boot setup

---
+++ src/main/java/org/psint/beyosclothing/BeyosclothingApplication.java
@@ -0,0 +1,13 @@
+package org.psint.beyosclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyosclothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyosclothingApplication.class, args);
+	}
+
+}
